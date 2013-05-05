1edd11a258cebdc5989d6e6128cd293adbedf547
Mini Model <minimodel@metamodel.me>
2013-05-05 05:11:26 +0200

    Updated translations from transifex.

---
+++ src/system/modules/metamodels/languages/fr/default.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Translations are managed using Transifex. To create a new translation
+ * or to help to maintain an existing one, please register at transifex.com.
+ *
+ * @link http://help.transifex.com/intro/translating.html
+ * @link https://www.transifex.com/projects/p/metamodels/language/fr/
+ *
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ *
+ * last-updated: 2013-05-04T02:11:31+02:00
+ */
+
+
+$GLOBALS['TL_LANG']['BRD']['metamodels']                     = 'MetaModels';
+$GLOBALS['TL_LANG']['CTE']['metamodel_content']['0']         = 'Liste des MetaModels';
+$GLOBALS['TL_LANG']['CTE']['metamodels_frontendfilter']['0'] = 'Filtre frontal pour MetaModel';
+$GLOBALS['TL_LANG']['CTE']['metamodels_frontendfilter']['1'] = 'Ajoute un frontend filtre pour un MetaModel.';
