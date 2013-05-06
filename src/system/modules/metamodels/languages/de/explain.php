3f83709807a81cbfa944c74a0cc69bc8dd3384b2
Mini Model <minimodel@metamodel.me>
2013-05-06 10:12:06 +0200

    Updated translations from transifex.

--- src/system/modules/metamodels/languages/de/explain.php
+++ src/system/modules/metamodels/languages/de/explain.php
@@ -8,7 +8,7 @@
  *
  * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
  *
- * last-updated: 2013-05-05T14:11:18+02:00
+ * last-updated: 2013-05-06T10:11:20+02:00
  */
 
 $GLOBALS['TL_LANG']['XPL']['customsql']['0']['0'] = 'Zusammenfassung';
@@ -21,7 +21,10 @@ $GLOBALS['TL_LANG']['XPL']['customsql']['1']['1'] = '<pre>SELECT id FROM mm_myme
 		Dieses selektiert alle IDs von der Tabelle <em>mm_mymetamodel</em> wo der Wert <em>page_id=1</em> ist.
 		';
 $GLOBALS['TL_LANG']['XPL']['customsql']['2']['0'] = 'Beispiel 2<br />Tabellennamen einsetzen';
+$GLOBALS['TL_LANG']['XPL']['customsql']['2']['1'] = '<pre>SELECT id FROM {{table}} WHERE page_id=1</pre>
+		Nahezu gleich wie in Beispiel 1, außer dass der Tabellenname des aktuellen MetaModels  (also: das <em>mm_mymetamodel</em> von oben) in die Abfrage eingefügt wird.		';
 $GLOBALS['TL_LANG']['XPL']['customsql']['3']['0'] = 'Inserttags';
+$GLOBALS['TL_LANG']['XPL']['customsql']['3']['1'] = 'Insert-Tags werden unterstützt. Bitte beachten, dass nicht alle Tags für alle Ausgaben verfügbar sein können. Falls eine Filtereinstellung wie zum Beispiel  <em>{{page::id}}</em> benutzt wird, dann ist der Insert-Tag nur für einen Seitenaufruf im Frontend und nicht für einen RRS-Feed verfügbar.';
 $GLOBALS['TL_LANG']['XPL']['customsql']['4']['0'] = 'Sichere Inserttags';
 $GLOBALS['TL_LANG']['XPL']['customsql']['6']['0'] = 'Beispiel 3<br />
 Komplexe Filer, Parameter und Quellen nutzen';
