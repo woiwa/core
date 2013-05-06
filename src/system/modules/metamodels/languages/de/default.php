40625c12c1383fa51038a290498d37455c705542
Mini Model <minimodel@metamodel.me>
2013-05-06 18:12:28 +0200

    Updated translations from transifex.

--- src/system/modules/metamodels/languages/de/default.php
+++ src/system/modules/metamodels/languages/de/default.php
@@ -8,39 +8,39 @@
  *
  * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
  *
- * last-updated: 2013-05-06T17:11:24+02:00
+ * last-updated: 2013-05-06T18:11:36+02:00
  */
 
 $GLOBALS['TL_LANG']['BRD']['metamodel_attribute']                                     = 'Attribute von "%s"';
 $GLOBALS['TL_LANG']['BRD']['metamodel_dca']                                           = 'Alle Eingabemasken für "%s"';
-$GLOBALS['TL_LANG']['BRD']['metamodel_dcasetting']                                    = 'Eingabemaske in "%s"';
-$GLOBALS['TL_LANG']['BRD']['metamodel_dcasetting_subpalette']                         = 'Sub Bereiche für "%s"';
+$GLOBALS['TL_LANG']['BRD']['metamodel_dcasetting']                                    = 'Eingabemasken in "%s"';
+$GLOBALS['TL_LANG']['BRD']['metamodel_dcasetting_subpalette']                         = 'Sub-Bereiche für "%s"';
 $GLOBALS['TL_LANG']['BRD']['metamodel_filter']                                        = 'Alle Filter von "%s"';
 $GLOBALS['TL_LANG']['BRD']['metamodel_filtersetting']                                 = 'Filtereinstellungen in "%s"';
 $GLOBALS['TL_LANG']['BRD']['metamodel_rendersetting']                                 = 'Rendereinstellungen in "%s"';
-$GLOBALS['TL_LANG']['BRD']['metamodel_rendersettings']                                = 'Rendereinstellungen von "%s"';
+$GLOBALS['TL_LANG']['BRD']['metamodel_rendersettings']                                = 'Alle Rendereinstellungen von "%s"';
 $GLOBALS['TL_LANG']['BRD']['metamodels']                                              = 'MetaModels';
 $GLOBALS['TL_LANG']['CTE']['metamodel_content']['0']                                  = 'MetaModel-Liste';
 $GLOBALS['TL_LANG']['CTE']['metamodel_content']['1']                                  = 'Fügt eine Liste aus MetaModel-Datensätzen dem Artikel hinzu.';
 $GLOBALS['TL_LANG']['CTE']['metamodels']                                              = 'MetaModel-Elemente';
 $GLOBALS['TL_LANG']['CTE']['metamodels_frontendfilter']['0']                          = 'MetaModel-Frontendfilter';
-$GLOBALS['TL_LANG']['CTE']['metamodels_frontendfilter']['1']                          = 'Fügt einen Frontend-Filter für ein MetaModel hinzu';
+$GLOBALS['TL_LANG']['CTE']['metamodels_frontendfilter']['1']                          = 'Fügt einen Frontend-Filter für ein MetaModel hinzu.';
 $GLOBALS['TL_LANG']['ERR']['activate_extension']                                      = 'Bitte aktivieren Sie die benötigte Erweiterung "%s" (%s).';
 $GLOBALS['TL_LANG']['ERR']['columnExists']                                            = 'Ein Attribut mit diesem Tabellennamen ist bereits vorhanden.';
 $GLOBALS['TL_LANG']['ERR']['install_extension']                                       = 'Bitte installieren Sie die benötigte Erweiterung "%s" (%s).';
 $GLOBALS['TL_LANG']['ERR']['invalidTableName']                                        = 'Der Tabellenname ist ungültig.';
-$GLOBALS['TL_LANG']['ERR']['no_attribute_extension']                                  = 'Bitte installieren Sie mindestens eine Attribut erweiterung! Metamodelle ohne Attribute sind nicht sinnvoll. ';
-$GLOBALS['TL_LANG']['ERR']['no_palette']                                              = 'Sie haben versucht, auf das MetaModel "%s" ohne eine Ansichtsoption für Benutzer %s zuzugreifen.';
+$GLOBALS['TL_LANG']['ERR']['no_attribute_extension']                                  = 'Bitte installieren Sie mindestens eine Attribut-Erweiterung! MetaModels ohne Attribute sind nicht sinnvoll. ';
+$GLOBALS['TL_LANG']['ERR']['no_palette']                                              = 'Sie haben versucht, auf das MetaModel "%s" ohne eine Eingabemaske für Benutzer %s zuzugreifen.';
 $GLOBALS['TL_LANG']['ERR']['no_view']                                                 = 'Sie haben versucht, auf das MetaModel "%s" ohne eine Ansichtsoption für Benutzer %s zuzugreifen.';
-$GLOBALS['TL_LANG']['ERR']['upgrade_php_version']                                     = 'Die Version der PHP-Interpreter ist zu niedrig, bitte ein Upgrade auf mindestens %s (Sie sind derzeit %s) ';
-$GLOBALS['TL_LANG']['MSC']['metamodel_edit_as_child']['label']                        = ' "%s" editieren für den EIntrag %%s';
-$GLOBALS['TL_LANG']['MSC']['metamodel_filtersetting']['editRecord']                   = 'Filtereinstellungen %%s für Filter "%s" in MetaModel "%" bearbeiten';
+$GLOBALS['TL_LANG']['ERR']['upgrade_php_version']                                     = 'Ihre PHP-Version ist zu niedrig, Bitte machen Sie ein Upgrade und mindestens Version %s (Sie benutzen zur Zeit %s) ';
+$GLOBALS['TL_LANG']['MSC']['metamodel_edit_as_child']['label']                        = ' "%s" für den EIntrag %%s editieren.';
+$GLOBALS['TL_LANG']['MSC']['metamodel_filtersetting']['editRecord']                   = 'Die Filtereinstellungen %%s für Filter "%s" in MetaModel "%s" bearbeiten.';
 $GLOBALS['TL_LANG']['MSC']['metamodel_filtersetting']['label']                        = 'Filter "%s" in MetaModel "%s"';
 $GLOBALS['TL_LANG']['MSC']['metamodel_filtersettings_parameter']['simplelookup']['0'] = 'Filterwert für das Attribut "%s"';
-$GLOBALS['TL_LANG']['MSC']['metamodel_rendersetting']['editRecord']                   = 'Attributeinstellungen %%s für Rendereinstellungen "%s" in MetaModel "%s" bearbeiten';
-$GLOBALS['TL_LANG']['MSC']['metamodel_rendersetting']['label']                        = 'Rendereinstellungen "%s" in MetaModel "%s"';
-$GLOBALS['TL_LANG']['MSC']['metamodels_contributor']                                  = 'Dank an diese Benutzer für Tickets, Anregungen und Übersetzungen ';
-$GLOBALS['TL_LANG']['MSC']['metamodels_help']                                         = 'Wir rufen für Ihre Hilfe auf!';
+$GLOBALS['TL_LANG']['MSC']['metamodel_rendersetting']['editRecord']                   = 'Die Attributeinstellungen %%s für Rendereinstellung "%s" in MetaModel "%s" bearbeiten.';
+$GLOBALS['TL_LANG']['MSC']['metamodel_rendersetting']['label']                        = 'Rendereinstellung "%s" in MetaModel "%s"';
+$GLOBALS['TL_LANG']['MSC']['metamodels_contributor']                                  = 'Wir danken diesen Benutzerinnen und Benutzern für Tickets, Anregungen und Übersetzungen:';
+$GLOBALS['TL_LANG']['MSC']['metamodels_help']                                         = 'Bitte unterstützen Sie uns!';
 $GLOBALS['TL_LANG']['MSC']['noItemsMsg']                                              = 'Ihre Suche lieferte keine passenden Ergebnisse.';
 $GLOBALS['TL_LANG']['MSC']['no_theme']                                                = 'Globaler Gültigkeitsbereich ';
 $GLOBALS['TL_LANG']['MSC']['panelLayout']['filter']['0']                              = 'Filter';
@@ -52,13 +52,13 @@ $GLOBALS['TL_LANG']['MSC']['template_in_theme']                                 
 $GLOBALS['TL_LANG']['MSC']['tl_class']['clr']['0']                                    = 'clr';
 $GLOBALS['TL_LANG']['MSC']['tl_class']['clr']['1']                                    = 'Hebt alle Floats auf.';
 $GLOBALS['TL_LANG']['MSC']['tl_class']['long']['0']                                   = 'long';
-$GLOBALS['TL_LANG']['MSC']['tl_class']['long']['1']                                   = 'Lässt das Eingabefeld zwei Spalten umspannen.';
+$GLOBALS['TL_LANG']['MSC']['tl_class']['long']['1']                                   = 'Sorgt dafür, dass das Eingabefeld zwei Spalten umfasst.';
 $GLOBALS['TL_LANG']['MSC']['tl_class']['m12']['0']                                    = 'm12';
 $GLOBALS['TL_LANG']['MSC']['tl_class']['m12']['1']                                    = 'Fügt dem Element einen oberen Abstand von 12 Pixeln hinzu (z.B. für einzelne Checkboxen).';
 $GLOBALS['TL_LANG']['MSC']['tl_class']['w50']['0']                                    = 'w50';
 $GLOBALS['TL_LANG']['MSC']['tl_class']['w50']['1']                                    = 'Setzt die Feldbreite auf 50% und floatet das Element (float:left).';
 $GLOBALS['TL_LANG']['MSC']['tl_class']['wizard']['0']                                 = 'wizard';
-$GLOBALS['TL_LANG']['MSC']['tl_class']['wizard']['1']                                 = 'Verkürzt das Eingabefeld, damit genug Platz für den Wizard (z.B. Date Picker) ist.';
+$GLOBALS['TL_LANG']['MSC']['tl_class']['wizard']['1']                                 = 'Verkürzt das Eingabefeld, damit genug Platz für den Wizard (z.B. einen Date Picker) ist.';
 $GLOBALS['TL_LANG']['metamodels_frontendfilter']['action_add']                        = '+';
 $GLOBALS['TL_LANG']['metamodels_frontendfilter']['action_remove']                     = '-';
 $GLOBALS['TL_LANG']['metamodels_frontendfilter']['do_not_filter']                     = 'Nicht filtern';
--- src/system/modules/metamodels/languages/de/modules.php
+++ src/system/modules/metamodels/languages/de/modules.php
@@ -8,7 +8,7 @@
  *
  * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
  *
- * last-updated: 2013-05-06T17:11:24+02:00
+ * last-updated: 2013-05-06T18:11:36+02:00
  */
 
 $GLOBALS['TL_LANG']['FMD']['metamodel_list']['0']            = 'MetaModel-Liste';
@@ -20,4 +20,4 @@ $GLOBALS['TL_LANG']['FMD']['metamodels_frontendfilter']['1'] = 'Fügt einen Fron
 $GLOBALS['TL_LANG']['MOD']['metamodels']['0']                = 'MetaModels';
 $GLOBALS['TL_LANG']['MOD']['metamodels']['1']                = 'Die MetaModels-Erweiterung ermöglicht es, eigene Datenmodelle zu erstellen';
 $GLOBALS['TL_LANG']['MOD']['support_metamodels']['0']        = 'MetaModels unterstützen';
-$GLOBALS['TL_LANG']['MOD']['support_metamodels']['1']        = 'Unterstützungs Informationen für die Metamodels Erweiterung.';
+$GLOBALS['TL_LANG']['MOD']['support_metamodels']['1']        = 'Informationen zur Unterstützung der MetaModels Extension.';
