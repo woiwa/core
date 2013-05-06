cbf0d309a6b9544fa19ae07a40a44f782687e4cd
Mini Model <minimodel@metamodel.me>
2013-05-06 20:12:14 +0200

    Updated translations from transifex.

--- src/system/modules/metamodels/languages/rm/modules.php
+++ src/system/modules/metamodels/languages/rm/modules.php
@@ -8,10 +8,16 @@
  *
  * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
  *
- * last-updated: 2013-05-05T11:43:40+02:00
+ * last-updated: 2013-05-06T20:11:26+02:00
  */
 
-
-$GLOBALS['TL_LANG']['FMD']['metamodel_list']['0'] = 'Glista da MetaModel';
-$GLOBALS['TL_LANG']['FMD']['metamodels']['0']     = 'MetaModels';
-$GLOBALS['TL_LANG']['MOD']['metamodels']['0']     = 'MetaModels';
+$GLOBALS['TL_LANG']['FMD']['metamodel_list']['0']            = 'Glista da MetaModel';
+$GLOBALS['TL_LANG']['FMD']['metamodel_list']['1']            = 'Agiuntescha ina glista dad elements da MetaModel ad ina pagina.';
+$GLOBALS['TL_LANG']['FMD']['metamodels']['0']                = 'MetaModels';
+$GLOBALS['TL_LANG']['FMD']['metamodels']['1']                = 'L\'extensiun MetaModels ta lubescha da crear agens models da datas.';
+$GLOBALS['TL_LANG']['FMD']['metamodels_frontendfilter']['0'] = 'Filter da front-end da MetaModel';
+$GLOBALS['TL_LANG']['FMD']['metamodels_frontendfilter']['1'] = 'Agiuntescha in filter da front-end per in MetaModel.';
+$GLOBALS['TL_LANG']['MOD']['metamodels']['0']                = 'MetaModels';
+$GLOBALS['TL_LANG']['MOD']['metamodels']['1']                = 'L\'extensiun MetaModels ta lubescha da crear agens models da datas.';
+$GLOBALS['TL_LANG']['MOD']['support_metamodels']['0']        = 'Agid per MetaModels';
+$GLOBALS['TL_LANG']['MOD']['support_metamodels']['1']        = 'Infurmaziuns d\'agid per l\'extensiun da MetaModels.';
---
+++ src/system/modules/metamodels/languages/rm/tl_module.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Translations are managed using Transifex. To create a new translation
+ * or to help to maintain an existing one, please register at transifex.com.
+ *
+ * @link http://help.transifex.com/intro/translating.html
+ * @link https://www.transifex.com/projects/p/metamodels/language/rm/
+ *
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ *
+ * last-updated: 2013-05-04T02:11:31+02:00
+ */
+
+
+$GLOBALS['TL_LANG']['tl_module']['metamodel']['0']                  = 'MetaModel';
+$GLOBALS['TL_LANG']['tl_module']['metamodel']['1']                  = 'Il MetaModel che duai vegnir enumerar en questa glista.';
+$GLOBALS['TL_LANG']['tl_module']['metamodel_filtering']['0']        = 'Configuraziun da filter per applitgar';
+$GLOBALS['TL_LANG']['tl_module']['metamodel_limit']['0']            = 'Dumber maximal dad elements';
+$GLOBALS['TL_LANG']['tl_module']['metamodel_limit']['1']            = 'Endatscha il dumber maximal dad elements. Endatescha 0 per mussar tut ils elements e pia deactivar la paginaziun.';
+$GLOBALS['TL_LANG']['tl_module']['metamodel_offset']['0']           = 'Offset da la glista';
+$GLOBALS['TL_LANG']['tl_module']['metamodel_offset']['1']           = 'Definescha la valur da l\'offset (p.ex. 10 per sursiglir ils emprims 10 elements).';
+$GLOBALS['TL_LANG']['tl_module']['metamodel_sortby']['0']           = 'Zavrar tenor';
+$GLOBALS['TL_LANG']['tl_module']['metamodel_sortby']['1']           = 'Tscherna la successiun da zavrar.';
+$GLOBALS['TL_LANG']['tl_module']['metamodel_sortby_direction']['0'] = 'Zavrar tenor direcziun';
+$GLOBALS['TL_LANG']['tl_module']['metamodel_sortby_direction']['1'] = 'Successiun ascendenta u descendenta.';
+$GLOBALS['TL_LANG']['tl_module']['metamodel_use_limit']['0']        = 'Utilisar in offset ed ina limita per la glista';
+$GLOBALS['TL_LANG']['tl_module']['metamodel_use_limit']['1']        = 'Activar, sche ti vuls limitar il dumber dad elements en la glista. Quai è practic sche ti vuls be mussar ils emprims 500 elements u excluder ils emprims 10 elements, ma laschar intact la paginaziun.';
+$GLOBALS['TL_LANG']['tl_module']['mm_filter_legend']                = 'Filter dal MetaModel';
